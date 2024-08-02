c3b3371533621957522dce0bbef548214d18d5f8
bartek <bartek.banach.47@gmail.com>
2024-08-02 22:44:21 +0200

    Create Url entity.

---
+++ src/Entity/Url.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Url entity.
+ */
+
+namespace App\Entity;
+
+use App\Repository\UrlRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Url.
+ */
+#[ORM\Entity(repositoryClass: UrlRepository::class)]
+#[ORM\Table(name: 'urls')]
+class Url
+{
+    /**
+     * Primary key.
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * Long URL.
+     *
+     * @var string|null the original long URL
+     */
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $longUrl = null;
+
+    /**
+     * Short URL.
+     *
+     * @var string|null the short URL code
+     */
+    #[ORM\Column(length: 30, nullable: true)]
+    private ?string $shortUrl = null;
+
+    /**
+     * Created at.
+     *
+     * @psalm-suppress PropertyNotSetInConstructor
+     */
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    /**
+     * Getter tfor ID.
+     *
+     * @return int|null the identifier of the URL entity
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter for long URL.
+     *
+     * @return string|null the original long URL
+     */
+    public function getLongUrl(): ?string
+    {
+        return $this->longUrl;
+    }
+
+    /**
+     * Setter for long URL.
+     *
+     * @param string $longUrl the original long URL
+     */
+    public function setLongUrl(string $longUrl): static
+    {
+        $this->longUrl = $longUrl;
+
+        return $this;
+    }
+
+    /**
+     * Getter for short URL.
+     *
+     * @return string|null the short URL code
+     */
+    public function getShortUrl(): ?string
+    {
+        return $this->shortUrl;
+    }
+
+    /**
+     * Setter for short URL.
+     *
+     * @param string|null $shortUrl the short URL code
+     */
+    public function setShortUrl(?string $shortUrl): static
+    {
+        $this->shortUrl = $shortUrl;
+
+        return $this;
+    }
+
+    /**
+     * Getter for createdAt.
+     *
+     * @return \DateTimeImmutable|null the date and time when the URL was created
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Setter for createdAt.
+     *
+     * @param \DateTimeImmutable $createdAt the date and time when the URL was created
+     */
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
